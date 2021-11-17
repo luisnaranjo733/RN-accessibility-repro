5cd48896573505da527b5b411629d63c280d0e69
Luis Naranjo <luisnaranjo733@gmail.com>
2021-11-16 16:03:29 -0800

    add a clean example

--- App.js
+++ App.js
@@ -1,20 +1,40 @@
+/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
-import {
-  Button,
-  View,
-} from 'react-native';
+import {Button, View, Text} from 'react-native';
 
 const App = () => {
-  return (<View style={{ flex: 1, justifyContent: "center", alignItems: "center", backgroundColor: "white"}}>
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center", backgroundColor: "blue"}} accessible={false}>
-      <Text>{"Sample text"}</Text>
-      <Button title="Sample button" onPress={() => {}}/>
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'white',
+      }}>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundColor: 'blue',
+        }}
+        accessible={false}>
+        <Text>{'Blue text'}</Text>
+        <Button title="Blue button" onPress={() => {}} />
+      </View>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundColor: 'red',
+        }}
+        accessible={true}>
+        <Text>{'Red text'}</Text>
+        <Button title="Red button" onPress={() => {}} />
+      </View>
     </View>
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center", backgroundColor: "red"}} accessible={true}>
-      <Text>{"Sample text"}</Text>
-      <Button title="Sample button" onPress={() => {}}/>
-    </View>
-  </View>)
+  );
 };
 
 export default App;
